14f876012d511a81a87c31d7f423af4f595aa32d
Ben Smith <ben@thesmith.co.uk>
2010-09-04 11:41:00 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jambox::Application.config.secret_token = '982b47153b2fa9ac425a5e7eec9611cc82fe0e9b5ff30241a4dd8de109237ff441986e187c6266145b148b53a2eb116de8c68930401d3bf9127f6d1b56754a87'
